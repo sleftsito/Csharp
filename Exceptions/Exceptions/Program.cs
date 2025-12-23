7b3ae6589688103d5d4124075aa300a55a0eb086
unknown <tomasuniversitario683@gmail.com>
2025-12-23 18:42:12 -0400

    all i did in the course

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Exceptions;
+
+try
+{
+    int[] arr = { };
+    int data = arr[0];
+}
+catch(FormatException ex) // como esta es la clase padre de todas las excepciones, puedes usar polimorfismo para poder llamar a todas. Sirve cuando no conoces el tipo de excepcion
+{
+    string msg = ex.Message;
+
+}
+catch(IndexOutOfRangeException ex)
+{
+    //string msg = ex.Message;
+    //Console.WriteLine(msg);
+    throw new ExcepcionPersonalizada("Hubo un error manin");
+}
+catch(Exception ex) // Si colocaste una excepcion que cubre ya la escepcion que cubres, no te deja colocarla
+{
+    string msg = ex.Message;
+    Console.WriteLine(msg);
+}
+finally //Siempre se ejecuta sin importar si hubo o no una excepcion
+{
+    Console.WriteLine("Siempre me ejecuto");
+}
+
